d50c095893cf3792d2397147ccd54825794f5d45
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-08 20:31:29 +0300

    feat: implement row validation

---
+++ validations/row.js
@@ -0,0 +1,15 @@
+import { body } from 'express-validator'
+
+export const rowCreateValidation = [
+  body('equipmentCosts', 'Enter the number').isNumeric(),
+  body('estimatedProfit', 'Enter the number').isNumeric(),
+  body('id', 'Enter the number').isNumeric(),
+  body('machineOperatorSalary', 'Enter the number').isNumeric(),
+  body('mainCosts', 'Enter the number').isNumeric(),
+  body('materials', 'Enter the number').isNumeric(),
+  body('mimExploitation', 'Enter the number').isNumeric(),
+  body('overheads', 'Enter the number').isNumeric(),
+  body('rowName', 'Enter the corrent name').isString(),
+  body('salary', 'Enter the number').isNumeric(),
+  body('total', 'Enter the number').isNumeric(),
+]
