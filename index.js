bd048a19aca78cddec578977876566fa3b0ab272
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-08 17:10:27 +0300

    feat: add mongodb to project

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express"
 import * as dotenv from 'dotenv' 
+import mongoose from 'mongoose'
 
 const app = express()
 
 dotenv.config()
 app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_CONNECT_LINK)
+  .then(() => { console.log('MongoDB connect OK!') })
+  .catch((err) => { console.log(`MongoDB connect Error: ${err}`) })
 
 const port = process.env.PORT || 4000
 
