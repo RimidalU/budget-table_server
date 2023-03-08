ea30f61b0533588e4bc24d3097a347ca4e8a0051
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-08 16:18:17 +0300

    feat: add base server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import express from "express";
 
 const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log({ err });
+    }
+    console.log(`Server start on port ${port}`);
+})
