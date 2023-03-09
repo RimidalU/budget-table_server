1a26fed9a76d35ccfd6414fd855bd80aed8187d1
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-09 13:55:34 +0300

    feat: implement CRUD metods for row

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express"
 import * as dotenv from 'dotenv' 
 import mongoose from 'mongoose'
 
+import { rowCreateValidation, rowUpdateValidation } from "./validations/row.js"
+import { RowController } from "./controllers/index.js"
+
+
 const app = express()
 
 dotenv.config()
@@ -12,10 +16,18 @@ mongoose.connect(process.env.MONGODB_CONNECT_LINK)
   .catch((err) => { console.log(`MongoDB connect Error: ${err}`) })
 
 const port = process.env.PORT || 4000
+const prefix = process.env.PREFIX
+
 
 app.get('/', (req, res) => {
     res.send('Hello!')
 })
+
+app.get(`${prefix}list`, RowController.getAll)
+app.post(`${prefix}create`, rowCreateValidation, RowController.create)
+app.delete(`${prefix}:id/delete`, RowController.remove)
+app.post(`${prefix}:id/update`, rowUpdateValidation, RowController.update)
+
 
 app.listen(port, (err) => {
     if (err) {
